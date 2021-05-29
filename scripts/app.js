0c5a2450e07edf7c405e046e7cc557b7274d567c
vtominc <vanja.tominc@gmail.com>
2021-05-29 11:41:08 +0100

    Progress so slow

--- scripts/app.js
+++ scripts/app.js
@@ -7,12 +7,37 @@ function init() {
   const cellCount = width * width
   const cells = []
 
-  const matrushkaStartingPosition = 0
-  let currentMatrushkaPosition = 0
+  const aMazeing = [
+    4, 3, 3, 3, 3, 2, 2, 2, 2, 3, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4,
+    3, 2, 2, 2, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 2, 2, 2, 2, 2, 3,
+    3, 2, 3, 3, 3, 2, 2, 2, 3, 3, 2, 2, 2, 3, 3, 3, 3, 3, 2, 3,
+    3, 2, 2, 2, 3, 2, 5, 2, 3, 3, 2, 5, 2, 3, 2, 3, 2, 3, 3, 3,
+    3, 2, 5, 2, 3, 2, 2, 2, 2, 3, 2, 2, 2, 3, 2, 3, 2, 3, 3, 2,
+    3, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 3, 2,
+    3, 3, 3, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2, 3, 3, 3, 3, 3, 3, 2,
+    3, 2, 3, 2, 2, 2, 2, 3, 3, 3, 1, 3, 3, 3, 2, 2, 2, 2, 3, 2,
+    2, 2, 3, 3, 3, 3, 2, 3, 2, 2, 6, 2, 2, 3, 2, 3, 3, 3, 3, 2,
+    2, 3, 3, 3, 2, 3, 2, 3, 2, 6, 6, 6, 2, 3, 3, 3, 2, 2, 3, 2,
+    2, 3, 2, 3, 2, 3, 3, 3, 2, 2, 2, 2, 2, 3, 2, 3, 3, 3, 3, 2,
+    3, 3, 2, 3, 3, 3, 2, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 3, 2,
+    3, 2, 2, 2, 2, 2, 2, 3, 2, 3, 3, 2, 2, 3, 2, 3, 2, 3, 3, 2, 
+    3, 3, 3, 3, 2, 3, 3, 3, 2, 3, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2,
+    2, 3, 2, 3, 3, 3, 2, 3, 2, 2, 3, 3, 3, 2, 2, 2, 3, 2, 2, 2,
+    3, 3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 2, 3, 2, 5, 2, 3, 3, 3, 2,
+    2, 3, 2, 2, 2, 2, 2, 3, 2, 3, 3, 2, 3, 2, 2, 2, 2, 3, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+    3, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 3,
+    4, 3, 3, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4
+  ]
+
+  // console.log(aMazeing.length)
+
+  const matrushkaStartingPosition = 389
+  let currentMatrushkaPosition = 389
   const matrushkaClass = 'matrushka'
 
-  const enemyStartingPosition = 0
-  let currentEnemyPosition = 0
+  const enemyStartingPosition = 150
+  let currentEnemyPosition = 150
   // console.log(currentEnemyPosition)
   const enemyClass = 'enemy'
 
@@ -22,12 +47,21 @@ function init() {
       cell.innerText = i                                // Making sure we see a number inside of a div
       // console.log('cell >', cell)
       maze.appendChild(cell)                            // Appending a cell as a child to the main div with the class of 'grid'
-      cells.push(cell)                           
+      cells.push(cell)    
+     
+      addObstacle(i)
+      // addLittleBabushka(i)
+      addBigBabushka(i)
     }
-    console.log('cells >', cells)
-    currentEnemyPosition = Math.floor(Math.random() * cells.length)
-    console.log('Current number >', currentEnemyPosition)
+    
+    // addMatrushka(currentMatrushkaPosition)
     addEnemy(currentEnemyPosition)
+    
+    console.log('cells >', cells)
+    // currentEnemyPosition = Math.floor(Math.random() * cells.length)
+    // console.log('Current number >', currentEnemyPosition)
+    // enemyMovement()
+    handleKeyUp(event)
 
     // addMatrushka(matrushkaStartingPosition)
     // enemyMovement()
@@ -52,96 +86,159 @@ function init() {
     // console.log('Random >>', random)
     // ! random direction movement
     // updating currentEnemyPosition with a random I've got
+    currentEnemyPosition = enemyStartingPosition
     currentEnemyPosition += random 
-    // if (currentEnemyPosition % width !== width - 1) {
-    //   currentEnemyPosition++
-    //   console.log('right')
-    //   console.log('currentEnemyPosition >', currentEnemyPosition)
-    // } 
-    // if (currentEnemyPosition % width !== 0) {
-    //   currentEnemyPosition--
-    //   console.log('left')
-    //   console.log('currentEnemyPosition >', currentEnemyPosition)
-    // } 
-    if (currentEnemyPosition >= width - 1) {         
+    if (currentEnemyPosition % width !== width - 1) {                 // ! RIGHT MOVE
+      currentEnemyPosition++
+      console.log('right')
+      console.log('currentEnemyPosition >', currentEnemyPosition)   
+    }
+    if (currentEnemyPosition % width !== 0) {                       // ! LEFT MOVE
+      currentEnemyPosition--
+      console.log('left')
+      console.log('currentEnemyPosition >', currentEnemyPosition)
+    }
+    if (currentEnemyPosition - width >= 0) {                   // ! UP MOVE
       currentEnemyPosition -= width
       console.log('up')
       console.log('currentEnemyPosition >', currentEnemyPosition)
     } 
-    // if (currentEnemyPosition + width <= width * width - 1) {
-    //   currentEnemyPosition += width
-    //   console.log('down')
-    //   console.log('currentEnemyPosition >', currentEnemyPosition)
-    // }
+    if (currentEnemyPosition + width <= width * width - 1) {   // ! DOWN
+      currentEnemyPosition += width
+      console.log('down')
+      console.log('currentEnemyPosition >', currentEnemyPosition)
+    }
     addEnemy(currentEnemyPosition)
   }
+  
+
   
   const movement = setInterval(() => {
     enemyMovement()
   }, 1000)
   clearTimeout(movement)
 
-  // function handleKeyUp(event) {
-  //   console.log('position before key', currentEnemyPosition)
-  //   removeEnemy(currentEnemyPosition)
 
-    // const key = event.keyCode
-    // if (key === 39 && currentEnemyPosition % width !== width - 1) {
-    //   console.log('RIGHT')
-    //   currentEnemyPosition++
-    // } else if (key === 37 && currentEnemyPosition % width !== 0) {
-    //   console.log('LEFT')
-    //   currentEnemyPosition--
-    // } else if (key === 38 && currentEnemyPosition >= width) {
-    //   console.log('UP')
-    //   currentEnemyPosition -= width
-    // } else if (key === 40 && currentEnemyPosition + width <= width * width - 1) {
-    //   console.log('DOWN')
-    //   currentEnemyPosition += width
-    // } else {
-    //   console.log('INVALID KEY')
-    // }
-
-    // addEnemy(currentEnemyPosition)
-  // }
 
   // ! MATRUSHKA
 
-  // function addMatrushka(position) {
-  //   cells[position].classList.add(matrushkaClass)
-  // }
+  function addMatrushka(position) {
+    cells[position].classList.add(matrushkaClass)
+  }
 
-  // function removeMatrushka(position) {
-  //   cells[position].classList.remove(matrushkaClass)
-  // }
+  function removeMatrushka(position) {
+    cells[position].classList.remove(matrushkaClass)
+  }
 
 
-  // function handleKeyUp(event) {
-  //   console.log('position before key', currentMatrushkaPosition)
-  //   const key = event.keyCode
-  //   removeMatrushka(currentMatrushkaPosition)
+  function handleKeyUp(event) {
+    console.log('position before key', currentMatrushkaPosition)
+    const key = event.keyCode
+    removeMatrushka(currentMatrushkaPosition)
 
-  //   if (key === 39 && currentMatrushkaPosition % width !== width - 1) {
-  //     console.log('RIGHT')
-  //     currentMatrushkaPosition++
-  //   } else if (key === 37 && currentMatrushkaPosition % width !== 0) {
-  //     console.log('LEFT')
-  //     currentMatrushkaPosition--
-  //   } else if (key === 38 && currentMatrushkaPosition >= width) {
-  //     console.log('UP')
-  //     currentMatrushkaPosition -= width
-  //   } else if (key === 40 && currentMatrushkaPosition + width <= width * width - 1) {
-  //     console.log('DOWN')
-  //     currentMatrushkaPosition += width
-  //   } else {
-  //     console.log('INVALID KEY')
+    if (key === 39) {                 // ! We are going right
+      console.log('RIGHT')
+      // We know that currentMatrushka doesn't have 'obstacle', but what about the next place, therefore we need to look into the future + 1
+      // But if the future doesn't have na obstacle, than we continue with our journey. 
+      if (cells[currentMatrushkaPosition + 1].classList.contains('obstacle')) {
+        currentMatrushkaPosition += 0
+      } else if (currentMatrushkaPosition % width !== width - 1) {
+        currentMatrushkaPosition++
+      }
+      // removeLittleBabushka()
+    } else if (key === 37 && currentMatrushkaPosition % width !== 0) {
+      console.log('LEFT')
+      currentMatrushkaPosition--
+      // removeLittleBabushka()
+    } else if (key === 38 && currentMatrushkaPosition >= width) {
+      console.log('UP')
+      currentMatrushkaPosition -= width
+      // removeLittleBabushka()
+    } else if (key === 40 && currentMatrushkaPosition + width <= width * width - 1) {
+      console.log('DOWN')
+      currentMatrushkaPosition += width
+      // removeLittleBabushka()
+    } else {
+      console.log('INVALID KEY')
+    }
+
+    addMatrushka(currentMatrushkaPosition)
+  }
+
+  // ! ADDING AND REMOVING ELEMENTS TO GRID
+
+  // I somehow need to find a way how to connect array aMazing with CSS and HTML. Maybe with for loop? And then compare if value is inside
+  // Checking if one value appears inside
+  // for (let i = 0; i < cells.length; i++) {
+  //   if (i === 2) {
+       //console.log('2 = true')
+  //   } else if (i === 3) {
+       // console.log('3 = true')
+  //   } else if (i === 4) {
+       // console.log('4 = true')
+  //   } else if (i === 5) {
+       // console.log('5 = true')
+  //   } else if (i === 6) {
+       // console.log('6 = true')
   //   }
-
-  //   addMatrushka(currentMatrushkaPosition)
   // }
+
+
+
+  // ? Checking for all cases of number 2
+  // const obstacles = []
+  // const element = 2
+  // let idx = aMazeing.indexOf(element)
+  // while (idx !== -1) {
+  //   obstacles.push(idx)
+  //   idx = aMazeing.indexOf(element, idx + 1)
+  // }
+  // console.log(obstacles)
+
+
+  // ! Adding obstacles
+  function addObstacle(index) {
+    if (aMazeing[index] === 2) {
+      cells[index].classList.add('obstacle')
+    }
+  } 
+
+  // ! Adding little Babushka, for now just some random coind
+  function addLittleBabushka(index) {
+    if (aMazeing[index] === 3) {
+      cells[index].classList.add('points')
+    }
+  }
+
+
+
+  // // ! Remove little Babushka
+
+  function removeLittleBabushka(index) {
+    if (aMazeing[index] === 3 && currentMatrushkaPosition) {
+      cells[index].classList.remove('points')
+    }
+  }
+
+  console.log('vanja', cells)
+  // function removeCat(position) {
+  //   cells[position].classList.remove(catClass)
+  // }
+
+  // ! Adding big Babushka, not yet properly styled
+  function addBigBabushka(index) {
+    if (aMazeing[index] === 4) {
+      cells[index].classList.add('bigPoints')
+    }
+  }
+
+
+
+
+
 
   // ! Event listener for keyboard keys.
-  // document.addEventListener('keyup', handleKeyUp)
+  document.addEventListener('keyup', handleKeyUp)
 
   // createGrid(matrushkaStartingPosition)
   // createGrid(enemyStartingPosition)
